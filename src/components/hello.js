facd5009f7bffb56e1ef18f71e2c763dc7f7808b
Ed <lookatemail@gmail.com>
2020-06-23 08:43:56 +0100

    Add file structure in src directory

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../css/hello.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import Hello from './components/hello';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
