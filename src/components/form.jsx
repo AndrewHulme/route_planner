fcc8b3335ec057866d4663f61453a246408c5479
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-23 13:55:41 +0100

    Adds API request for the openrouteservice

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -5,20 +5,57 @@ class Form extends Component {
 
   startChangeHandler = (event) => {
     this.setState({
-      startingpoint: event.target.value
-    })
-  }
+      startingpoint: event.target.value,
+    });
+  };
 
   endChangeHandler = (event) => {
     this.setState({
-      endpoint: event.target.value
-    })
-  }
+      endpoint: event.target.value,
+    });
+  };
 
   submitHandler = (event) => {
     event.preventDefault();
-    console.log(this.state)
-  }
+    console.log(this.state);
+
+    var apiKey = process.env.REACT_APP_ROUTE_API_KEY;
+    var transportType = "driving-car";
+    var startCoordinates = "8.681495,49.41461";
+    var endCoordinates = "8.687872,49.420318";
+    // console.log(apiKey);
+
+    // // Starting coordinates
+    // fetch(
+    //   `https://api.openrouteservice.org/geocode/search?api_key=` +
+    //     apiKey +
+    //     `&text=Piccadilly%20Circus`
+    // )
+    //   // We get the API response and receive data in JSON format...
+    //   .then((response) => response.json())
+    //   // ...then we update the users state
+    //   .then((data) => console.log(data))
+    //   // Catch any errors we hit and update the app
+    //   .catch((error) => this.setState({ error, isLoading: false }));
+
+    // Directions request
+    fetch(
+      `https://api.openrouteservice.org/v2/directions/` +
+        transportType +
+        `?api_key=` +
+        apiKey +
+        `&start=` +
+        startCoordinates +
+        `&end=` +
+        endCoordinates
+    )
+      // We get the API response and receive data in JSON format...
+      .then((response) => response.json())
+      // ...then we update the users state
+      .then((data) => console.log("Directions Request Made"))
+      // Catch any errors we hit and update the app
+      .catch((error) => this.setState({ error, isLoading: false }));
+  };
 
   render() {
     return (
@@ -26,14 +63,30 @@ class Form extends Component {
         <form onSubmit={this.submitHandler}>
           <div className="form-group">
             <label>Starting Point:</label>
-            <input autoFocus className='form-control' type="text" name="startingpoint" onChange={this.startChangeHandler}/>
+            <input
+              autoFocus
+              className="form-control"
+              type="text"
+              name="startingpoint"
+              onChange={this.startChangeHandler}
+            />
           </div>
           <div className="form-group">
             <label>End Point:</label>
-            <input className='form-control' type="text" name="endpoint" onChange={this.endChangeHandler}/>
+            <input
+              className="form-control"
+              type="text"
+              name="endpoint"
+              onChange={this.endChangeHandler}
+            />
           </div>
           <br />
-          <input className='form-control' type="submit" className="btn btn-primary" value="Generate" />
+          <input
+            className="form-control"
+            type="submit"
+            className="btn btn-primary"
+            value="Generate"
+          />
         </form>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-import Hello from "./components/hello";
+// import Hello from "./components/hello";
 import Form from "./components/form";
 import "bootstrap/dist/css/bootstrap.css";
 
