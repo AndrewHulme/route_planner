4fe45e424dd639515631d8e1647cd099fe2979f7
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-23 10:48:51 +0100

    Submitted form data is saved in component state

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,17 +2,35 @@ import React, { Component } from "react";
 
 class Form extends Component {
   state = {};
+
+  startChangeHandler = (event) => {
+    this.setState({
+      startingpoint: event.target.value
+    })
+  }
+
+  endChangeHandler = (event) => {
+    this.setState({
+      endpoint: event.target.value
+    })
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.submitHandler}>
           <div className="form-group">
             <label>Starting Point:</label>
-            <input autoFocus className='form-control' type="text" name="startingpoint" />
+            <input autoFocus className='form-control' type="text" name="startingpoint" onChange={this.startChangeHandler}/>
           </div>
           <div className="form-group">
             <label>End Point:</label>
-            <input className='form-control' type="text" name="endpoint" />
+            <input className='form-control' type="text" name="endpoint" onChange={this.endChangeHandler}/>
           </div>
           <br />
           <input className='form-control' type="submit" className="btn btn-primary" value="Generate" />
