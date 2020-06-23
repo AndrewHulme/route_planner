382f7fcecf392436f3153cf6fc020867f6ca93f3
Jed Stanton <jedstanton@hotmail.com>
2020-06-23 12:10:47 +0100

    Current location Geolocation added

---
+++ src/components/map.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Using geolocation JavaScript API in React</h4>
+      </div>
+    );
+  }
+}
+
+export default Map;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import Hello from './components/hello';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import Hello from "./components/hello";
+import Map from "./components/map";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Hello />
+    <Map />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
