af66e88ff47d343b3f437acf807ef6b440770459
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-23 15:06:10 +0100

    Add asynchronous function to geocode user input and save coordinates in state

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -17,47 +17,50 @@ class Form extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    console.log(this.state);
 
     var apiKey = process.env.REACT_APP_ROUTE_API_KEY;
     var transportType = "driving-car";
     var startCoordinates = "8.681495,49.41461";
     var endCoordinates = "8.687872,49.420318";
-    // console.log(apiKey);
+    var startingURL = 'https://eu1.locationiq.com/v1/search.php?key=55968608b06de7&q=Piccadilly%20Circus&format=json';
+    var endingURL = 'https://eu1.locationiq.com/v1/search.php?key=55968608b06de7&q=Waterloo&format=json';
+    var routeURL= `https://api.openrouteservice.org/v2/directions/` +
+      transportType +
+      `?api_key=` +
+      apiKey +
+      `&start=` +
+      startCoordinates +
+      `&end=` +
+      endCoordinates;
 
-    // // Starting coordinates
-    // fetch(
-    //   `https://api.openrouteservice.org/geocode/search?api_key=` +
-    //     apiKey +
-    //     `&text=Piccadilly%20Circus`
-    // )
-    //   // We get the API response and receive data in JSON format...
-    //   .then((response) => response.json())
-    //   // ...then we update the users state
-    //   .then((data) => console.log(data))
-    //   // Catch any errors we hit and update the app
-    //   .catch((error) => this.setState({ error, isLoading: false }));
+      const asyncWrapper = async () => {
+        await fetch(startingURL)
+          .then(response => response.json())
+          .then(data => this.setState({
+            startingLat: data[0].lat,
+            startingLon: data[0].lon
+          }))
+        await fetch(endingURL)
+          .then(response => response.json())
+          .then(data => this.setState({
+            endingLat: data[0].lat,
+            endingLon: data[0].lon
+          }))
+        await fetch(routeURL)
+          // We get the API response and receive data in JSON format...
+          .then((response) => response.json())
+          // ...then we update the users state
+          .then((data) => console.log("Directions Request Made"))
+          // Catch any errors we hit and update the app
+          .catch((error) => this.setState({ error, isLoading: false }));
+      }
 
-    // Directions request
-    fetch(
-      `https://api.openrouteservice.org/v2/directions/` +
-        transportType +
-        `?api_key=` +
-        apiKey +
-        `&start=` +
-        startCoordinates +
-        `&end=` +
-        endCoordinates
-    )
-      // We get the API response and receive data in JSON format...
-      .then((response) => response.json())
-      // ...then we update the users state
-      .then((data) => console.log("Directions Request Made"))
-      // Catch any errors we hit and update the app
-      .catch((error) => this.setState({ error, isLoading: false }));
+      asyncWrapper();
+
   };
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <form onSubmit={this.submitHandler}>
