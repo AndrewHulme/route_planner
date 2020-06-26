9eec6660068816ccd922c756042c25fb9a6bd725
Jed Stanton <jedstanton@hotmail.com>
2020-06-26 11:26:27 +0100

    Graphhopper added

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -1,28 +1,34 @@
 import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
+import "lrm-graphhopper";
 import { withLeaflet } from "react-leaflet";
 
 class Routing extends MapLayer {
   createLeafletElement() {
-    const { map, startingCoords, endingCoords } = this.props;
-    console.log("YOOOOO");
-    console.log(startingCoords);
-    console.log(endingCoords);
+    const { map, startingCoords, endingCoords, vehicle } = this.props;
+    // console.log("YOOOOO");
+    // console.log(startingCoords);
+    // console.log(endingCoords);
+    // console.log(this.props.vehicle);
+
+    var apiGraphHopper = process.env.REACT_APP_GRAPHHOPPER;
 
     // var lat = localStorage.getItem("lat");
     // var lng = localStorage.getItem("long");
 
     let leafletElement = L.Routing.control({
       waypoints: [
-        // L.latLng(51.627534, -0.391221),
-        // L.latLng(51.645046, -0.388907),
-        // L.latLng(51.627534, -0.391221)
         L.latLng(startingCoords[0], startingCoords[1]),
         L.latLng(endingCoords[0], endingCoords[1]),
         // L.latLng(27.68, 85.321),
         // L.latLng(27.7, 85.331),
       ],
+      router: L.Routing.graphHopper(apiGraphHopper, {
+        urlParameters: {
+          vehicle: "car",
+        },
+      }),
     }).addTo(map.leafletElement);
     return leafletElement.getPlan();
   }
--- src/components/routingRoundTrip.jsx
+++ src/components/routingRoundTrip.jsx
@@ -2,30 +2,37 @@ import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import { withLeaflet } from "react-leaflet";
+import "lrm-graphhopper";
 
 class RoutingRoundTrip extends MapLayer {
   createLeafletElement() {
     const { map, roundTripCoords } = this.props;
+    var apiGraphHopper = process.env.REACT_APP_GRAPHHOPPER;
 
-    let routingObj = {}
-    let waypointsArr = []
+    let routingObj = {};
+    let waypointsArr = [];
     roundTripCoords.forEach((item) => {
       let coord = L.latLng(item[1], item[0]);
-      waypointsArr.push(coord)
+      waypointsArr.push(coord);
     });
     routingObj.waypoints = waypointsArr;
 
-    let leafletElement = L.Routing.control(
-      routingObj
-    //   {
-    //   waypoints: [
-    //     L.latLng(51.627534, -0.391221),
-    //     L.latLng(51.627602, -0.390801),
-    //     L.latLng(51.627745, -0.389936),
-    //     L.latLng(51.627846, -0.389318),
-    //   ],
-    // }
-  ).addTo(map.leafletElement);
+    let leafletElement = L.Routing.control({
+      routingObj,
+      // //   {
+      // waypoints: [
+      //   L.latLng(51.627534, -0.391221),
+      //   L.latLng(51.627602, -0.390801),
+      //   L.latLng(51.627745, -0.389936),
+      //   L.latLng(51.627846, -0.389318),
+      // ],
+
+      router: L.Routing.graphHopper(apiGraphHopper, {
+        urlParameters: {
+          vehicle: "car",
+        },
+      }),
+    }).addTo(map.leafletElement);
     return leafletElement.getPlan();
   }
 }
