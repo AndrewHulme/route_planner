97f72d7eb314995ced8267451ed8724dd0d43f72
Ed <lookatemail@gmail.com>
2020-06-27 08:14:49 +0100

    Add css style sheet to index page and css file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./css/index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
