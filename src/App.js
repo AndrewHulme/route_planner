5aa0c503d5f620fd351f9d880224f24d7b62d803
Ed <lookatemail@gmail.com>
2020-06-28 20:03:37 +0100

    Able to login user to view app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/app.css';
-import Form from './components/form';
-import fire from './components/firebase';
-import Login from './components/login';
+import fire from './components/firebase.jsx';
+import Form from './components/form.jsx';
+import Login from './components/login.jsx';
 
-function App() {
-  // state = {};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+    this.authListener = this.authListener.bind(this);
+  }
 
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ? <Form /> : <Login />}
+        {/* <Login />
+        <Form /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
