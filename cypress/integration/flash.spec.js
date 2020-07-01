52f4cf28d48468d7d53017f368a41b2165d97278
Ed <lookatemail@gmail.com>
2020-07-01 17:48:18 +0100

    Test add for flash message

---
+++ cypress/integration/flash.spec.js
@@ -0,0 +1,16 @@
+describe('Flash', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+  it('should be able to have flash message in DOM', () => {
+    cy.get('input[name="roundTripStart"]').type('London Eye');
+    cy.get('input[name="roundTripLength"]').type('hello');
+    cy.get('select[cy-name="vehicleChoice"]').select('Cycling');
+    cy.get('#roundTripButton').click();
+    cy.get('.alert').should('have.text', '×Error no address could be found.');
+    cy.get('.close').click();
+    cy.get('.alert').should('not.exist');
+  });
+});
