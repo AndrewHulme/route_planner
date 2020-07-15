b2493b09c137afb48729190c49120f0aa3a9a475
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-07-15 16:43:30 +0100

    Refactor: made delete button into a functional component

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 
-class DeleteButton extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+function DeleteButton(props) {
     return (
       <button
         type="button"
         className="close"
-        onClick={(event) => this.props.removeMap(event)}
+        onClick={(event) => props.removeMap(event)}
       >
         <DeleteIcon />
       </button>
     )
-  }
 }
 
 export default DeleteButton;
