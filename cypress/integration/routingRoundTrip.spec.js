a82a1646d81902fcebf00add73d34189ef9758f8
Ed <lookatemail@gmail.com>
2020-08-11 12:45:50 +0100

    Fix cypress test for roundTripSpec

--- cypress/integration/routingRoundTrip.spec.js
+++ cypress/integration/routingRoundTrip.spec.js
@@ -1,38 +1,38 @@
 /// <reference types='cypress' />
 
-Cypress.on("uncaught:exception", (err, runnable) => {
+Cypress.on('uncaught:exception', (err, runnable) => {
   console.log(err);
   return false;
 });
 
-describe("Routing", () => {
-  context("Network Requests", () => {
+describe('Routing', () => {
+  context('Network Requests', () => {
     beforeEach(() => {
-      cy.visit("http://localhost:3000");
+      cy.visit('http://localhost:3000');
     });
-    it("able to submit new round route", () => {
-      const startInput = "London Eye";
+    it('able to submit new round route', () => {
+      const startInput = 'London Eye';
       const length = 10000;
 
       cy.get('input[name="startingPoint"]').type(startInput);
       cy.get('input[name="roundTripLength"]').type(length);
-      cy.get("#roundTripButton").click();
+      cy.get('#roundTripButton').click();
       cy.server().should((server) => {
-        expect(server.method).to.eq("GET");
+        expect(server.method).to.eq('GET');
         expect(server.status).to.eq(200);
       });
     });
 
-    it("contains my location", () => {
+    it('contains my location', () => {
       const length = 10000;
 
-      cy.get("#roundTripMyLocation").click();
+      cy.get('#useMyLocation').click();
       cy.get('input[name="roundTripLength"]').type(length);
       cy.get('select[cy-name="vehicleChoice"]')
-        .select("Walking")
-        .should("have.value", "foot");
-      cy.get("#roundTripButton").click();
-      cy.get('input[name="startingPoint"]').should("have.value", "My Location");
+        .select('Walking')
+        .should('have.value', 'foot-walking');
+      cy.get('#roundTripButton').click();
+      cy.get('input[name="startingPoint"]').should('have.value', 'My Location');
     });
   });
 });
